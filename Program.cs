b8e48e1934f792fc345713bd6c5a8c0a5f489b09
phucltv <phucltv@users.noreply.github.com>
2026-05-17 17:45:19 +0700

    Update version 3

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World Version 2!");
+app.MapGet("/", () => "Hello World Version 3!");
 
 app.Run();
